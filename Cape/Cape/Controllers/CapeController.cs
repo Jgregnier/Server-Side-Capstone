fe170ee0eb4713587070cfb98c4928c2e4e317b4
James Regnier <jgreg071@gmail.com>
2016-12-12 21:55:13 -0600

    Cleaned up unused identity code, set up basic auth in Cape Controller, did ver minor work on initial views

---
+++ Cape/Cape/Controllers/CapeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Cape.Models;
+using Cape.Repositories;
+
+namespace Cape.Controllers
+{
+    public class CapeController : Controller
+    {
+        public CapeController()
+        { }
+
+        public CapeController(UserRepository _repository)
+        {
+            userRepositry = _repository;
+        }
+
+        private UserRepository userRepositry;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Reports()
+        {
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Upload()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Cape/Cape/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cape.Models;
+
+namespace Cape.ViewModels
+{
+    public class BaseViewModel
+    {
+        protected ApplicationUser _User;
+
+        public BaseViewModel(ApplicationUser User)
+        {
+            _User = User;
+        }
+        public BaseViewModel()
+        { }
+
+    }
+}
